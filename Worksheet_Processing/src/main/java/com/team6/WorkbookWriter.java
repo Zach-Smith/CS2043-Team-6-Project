f9e91abed63ac1612246c753b36268d9117ed607
simon <simon@myPC>
2018-04-03 20:54:00 -0300

    The writer now creates a new Row if there is none for a Teacher.

--- Worksheet_Processing/src/main/java/com/team6/WorkbookWriter.java
+++ Worksheet_Processing/src/main/java/com/team6/WorkbookWriter.java
@@ -2,6 +2,7 @@ package com.team6;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
@@ -31,8 +32,12 @@ public class WorkbookWriter {
 
 		DataFormatter formatter = new DataFormatter();
 		String currentPeriod = "";
+		boolean teacherFound = false;
+		int[] startCollumsPeriodes = new int[5];
+		int templateRowIndex = 0;
 		
 		int worksheet = termStartMonth - month;
+	
 		
 		FileInputStream file = new FileInputStream(xlsFilePath);
 		 XSSFWorkbook workbook = new XSSFWorkbook(file);
@@ -45,13 +50,23 @@ public class WorkbookWriter {
 			String cellValue = formatter.formatCellValue(row.getCell(STARTING_CELL));
 			if (cellValue.equals("Period 1")) {
 				currentPeriod = "Period 1";
+				startCollumsPeriodes[0] = currentRow;
 			} else if (cellValue.equals("Period 2")) {
 				currentPeriod = "Period 2";
+				startCollumsPeriodes[1] = currentRow;
 			} else if (cellValue.equals("Period 3")) {
 				currentPeriod = "Period 3";
+				startCollumsPeriodes[2] = currentRow;
 			} else if (cellValue.equals("Period 4")) {
 				currentPeriod = "Period 4";
+				startCollumsPeriodes[3] = currentRow;
+			} else if (cellValue.equals("Period 5")) {
+				currentPeriod = "Period 5";
+				startCollumsPeriodes[4] = currentRow;
+			}else if (cellValue.equals("down't write in this row")) {
+				templateRowIndex = currentRow;
 			}
+	
 
 			if (cellValue.equals(teacherName) && currentPeriod.equals(period)) {
 				for (short currentCell = STARTING_CELL; currentCell < row.getLastCellNum(); currentCell++) {
@@ -61,13 +76,123 @@ public class WorkbookWriter {
 					int weekendDays = (day / 5) *2; 
 					if (currentCell - 1 == day + weekendDays) {
 						cell.setCellValue(1);
+						teacherFound = true;
 					}
 				}
 
 			}
 		}
+		if (teacherFound == false) {
+			createNewRow(workbook, sheet, startCollumsPeriodes,teacherName,period,day,templateRowIndex);
+			System.out.println("A new row for the teacher has been created.");
+		}
 		return workbook;
 	}
+	
+private static void createNewRow(Workbook workbook, XSSFSheet sheet, int[] startCollumsPeriodes,String teacherName, String period,
+		int day, int templateRowIndex) {
+	int indexForNewRow = 0;
+	if (period.equals("Period 1")) {
+		indexForNewRow = startCollumsPeriodes[0] -2;
+	} else if (period.equals("Period 2")) {
+		indexForNewRow = startCollumsPeriodes[0] -2;
+	} else if (period.equals("Period 3")) {
+		indexForNewRow = startCollumsPeriodes[0] -2;
+	} else if (period.equals("Period 4")) {
+		indexForNewRow = startCollumsPeriodes[0] -2;
+	} else if (period.equals("Period 5")) {
+		indexForNewRow = startCollumsPeriodes[0] -2;
+	}
+	//Row templateRow = sheet.getRow(templateRowIndex);
+	sheet.shiftRows(indexForNewRow,sheet.getLastRowNum(),1);   
+	//Row newRow = sheet.createRow(indexForNewRow);
+	
+	copyRow(workbook,  sheet, templateRowIndex, indexForNewRow);
+	
+	//for (short currentCell = 0; currentCell < templateRow.getLastCellNum(); currentCell++) 
+	
+}
+
+	private static void copyRow(Workbook workbook, Sheet worksheet, int sourceRowNum, int destinationRowNum) {
+        // Get the source / new row
+        Row newRow = worksheet.getRow(destinationRowNum);
+        Row sourceRow = worksheet.getRow(sourceRowNum);
+
+        // If the row exist in destination, push down all rows by 1 else create a new row
+        if (newRow != null) {
+            worksheet.shiftRows(destinationRowNum, worksheet.getLastRowNum(), 1);
+        } else {
+            newRow = worksheet.createRow(destinationRowNum);
+        }
+
+        // Loop through source columns to add to new row
+        for (int i = 0; i < sourceRow.getLastCellNum(); i++) {
+            // Grab a copy of the old/new cell
+            Cell oldCell = sourceRow.getCell(i);
+            Cell newCell = newRow.createCell(i);
+
+            // If the old cell is null jump to next cell
+            if (oldCell == null) {
+                newCell = null;
+                continue;
+            }
+
+            // Copy style from old cell and apply to new cell
+            CellStyle newCellStyle = workbook.createCellStyle();
+            newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
+            ;
+            newCell.setCellStyle(newCellStyle);
+
+            // If there is a cell comment, copy
+            if (oldCell.getCellComment() != null) {
+                newCell.setCellComment(oldCell.getCellComment());
+            }
+
+            // If there is a cell hyperlink, copy
+            if (oldCell.getHyperlink() != null) {
+                newCell.setHyperlink(oldCell.getHyperlink());
+            }
+
+            // Set the cell data type
+            newCell.setCellType(oldCell.getCellType());
+
+            // Set the cell data value
+            switch (oldCell.getCellType()) {
+                case Cell.CELL_TYPE_BLANK:
+                    newCell.setCellValue(oldCell.getStringCellValue());
+                    break;
+                case Cell.CELL_TYPE_BOOLEAN:
+                    newCell.setCellValue(oldCell.getBooleanCellValue());
+                    break;
+                case Cell.CELL_TYPE_ERROR:
+                    newCell.setCellErrorValue(oldCell.getErrorCellValue());
+                    break;
+                case Cell.CELL_TYPE_FORMULA:
+                    newCell.setCellFormula(oldCell.getCellFormula());
+                    break;
+                case Cell.CELL_TYPE_NUMERIC:
+                    newCell.setCellValue(oldCell.getNumericCellValue());
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    newCell.setCellValue(oldCell.getRichStringCellValue());
+                    break;
+            }
+        }
+
+        // If there are are any merged regions in the source row, copy to new row
+        for (int i = 0; i < worksheet.getNumMergedRegions(); i++) {
+            CellRangeAddress cellRangeAddress = worksheet.getMergedRegion(i);
+            if (cellRangeAddress.getFirstRow() == sourceRow.getRowNum()) {
+                CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
+                        (newRow.getRowNum() +
+                                (cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow()
+                                        )),
+                        cellRangeAddress.getFirstColumn(),
+                        cellRangeAddress.getLastColumn());
+                worksheet.addMergedRegion(newCellRangeAddress);
+            }
+        }
+    }
 	
 private static int getWorksheetNumber (int month) {
 		int worksheet = 0;
